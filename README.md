5a0e5e7577effdd0cd2d4ecaf10bf34290074a4c
zhoubing <zhoubing19820720@hotmail.com>
2022-10-04 22:47:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://blog.csdn.net/weixin_55626853/article/details/123941565
---
+++ app/src/main/java/com/example/opencvdemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.opencvdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.osgi.OpenCVNativeLoader;
+
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+        OpenCVNativeLoader openCVNativeLoader = new OpenCVNativeLoader();
+        openCVNativeLoader.init();
+        ByteBuffer buffer = ByteBuffer.allocate(bitmap.getByteCount());
+        bitmap.copyPixelsToBuffer(buffer);
+        Mat mat = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8UC4, buffer);
+        Log.d("OpenCV", "mat channels: " + mat.channels() + " cols: " + mat.cols() + " rows: " + mat.rows());
+    }
+}
